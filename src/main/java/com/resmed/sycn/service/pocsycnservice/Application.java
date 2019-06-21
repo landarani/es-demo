11f269b98424e5df463473d540393f4743799170
Shahram Landarani <shahram.landarani@resmed.com.au>
2019-06-21 16:58:51 +1000

    more settings added

--- src/main/java/com/resmed/sycn/service/pocsycnservice/Application.java
+++ src/main/java/com/resmed/sycn/service/pocsycnservice/Application.java
@@ -1,8 +1,5 @@
 package com.resmed.sycn.service.pocsycnservice;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -17,6 +14,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class Application {
 
@@ -30,10 +30,14 @@ public class Application {
     public class ElasticsearchConfiguration {
 
         @Bean
-        public Client client(@Value("${elasticsearch.host}") String esHost, @Value("${elasticsearch.port:9300}") int esPort,
-            @Value("${elasticsearch.sniff:false}") boolean esSniff)
+        public Client client(@Value("${elasticsearch.host:localhost}") String esHost, @Value("${elasticsearch.port:9300}") int esPort,
+            @Value("${elasticsearch.sniff:false}") boolean esSniff, @Value("${elasticsearch.node:elasticsearch}") String esNode,
+            @Value("{elasticsearch.credential:}") String credential)
             throws UnknownHostException {
-            TransportClient client = new PreBuiltTransportClient(Settings.builder().put("client.transport.sniff", esSniff).build());
+            TransportClient client =
+                new PreBuiltTransportClient(Settings.builder().put("client.transport.sniff", esSniff).put("cluster.name", esNode)
+                    .put("client.transport.ignore_cluster_name", true).put("client.transport.ping_timeout", "60s")
+                    .put("discovery.type", "single-node").put("request.headers.X-Found-Cluster", esNode).build());
             TransportAddress address = new TransportAddress(InetAddress.getByName(esHost), esPort);
             client.addTransportAddress(address);
             return client;
